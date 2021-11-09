d90a8de330e90469c822583cd2ad0f23c526faef
Douglas <douglas.silva.702@ufrn.edu.br>
2021-11-09 16:41:10 -0300

    Correção Questão 02

--- Questao02/Questao02.c
+++ Questao02/Questao02.c
@@ -8,22 +8,22 @@ int main(void){
     q = &j;
 
 //Expressao 1:
-    printf("p == &i = %d\n", p == &i);
+    printf("p == &i = %d\n\n", p == &i);
 
 //Expressao 2:
-     printf("*p - *q = %d\n", *p - *q);
+     printf("*p - *q = %d\n\n", *p - *q);
 
 //Expressao 3:
-    printf("**&p = %d\n", **&p);
+    printf("**&p = %d\n\n", **&p);
 
 //Expressao 4:
-    printf("3 - *p/(*q) + 7 = %d\n", 3 - *p/(*q) + 7);
+    printf("3 - *p/(*q) + 7 = %d\n\n", 3 - *p/(*q) + 7);
 
 //Observacao:
 /* A operacao acima considerara apenas os valores inteiros, por isso o resultado sera 10. Se desejar obter o valor real da operacao,
 a seguinte implementacao deve ser realizada:*/
 
-     printf("3 - *p/(*q) + 7 = %d\n", 3 - (double) *p/(*q) + 7);
+     printf("3 - *p/(*q) + 7 = %f\n\n", 3 - (float) *p/(*q) + 7);
 
 
 return 0;
