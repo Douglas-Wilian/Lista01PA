380aac80b42ded576b53f6bdb06459cacf237e0a
Douglas <douglas.silva.702@ufrn.edu.br>
2021-11-23 23:09:21 -0300

    Pequena correção Q20

--- Questão_20/TesteLibgc.c
+++ Questão_20/TesteLibgc.c
@@ -7,7 +7,7 @@ int main(void){
 int n;
 float tempo1=0, tempo2=0;
 int *p;
-printf("Digite 1 para testar a Libgc e 2 para a alocacao normal:\n");
+printf("Digite 1 para testar a Libgc:\n");
 scanf("%d", &n);
 
 while(n==1){
@@ -32,11 +32,11 @@ t = clock();
 printf("O tempo é %ld clicks = (%f segundos)\n", t, ((float)t)/CLOCKS_PER_SEC);
 tempo1=((float)t)/CLOCKS_PER_SEC;
 
-printf("Digite 1 para testar a Libgc e 2 para a alocacao normal:\n");
+printf("Digite 1 para testar a Libgc novamente e 2 para a alocacao normal:\n");
 scanf("%d", &n);	
 
 }
-
+while(n==2){
 clock_t t;
 t=clock();
 
@@ -58,6 +58,9 @@ t = clock();
 printf("O tempo é %ld clicks = (%f segundos)\n", t, ((float)t)/CLOCKS_PER_SEC);	
 tempo2 = ((float)t)/CLOCKS_PER_SEC;
 
+printf("Digite 2 para testar a alocacao normal novamente e 3 para obter os resultados:\n");
+scanf("%d", &n);
+}
 printf("Tempo Libgc = %f\n", tempo1);
 printf("Tempo Malloc = %f\n", tempo2);
 return 0;
